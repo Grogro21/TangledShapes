6660c3fcbb5ee94543154805bf984d7ca79ba05e
Grogro21 <74548874+Grogro21@users.noreply.github.com>
2024-11-05 12:18:36 +0100

    modelIncomplet

---
+++ ShapeSMOdel/Shape.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+
+namespace ShapeSMOdel
+{
+    public class Shape
+    {
+        private string _name;
+        private Point _position;
+        private string _theme;
+
+        public Shape(string name, Point position, string theme)
+        {
+            _name = name;
+            _position = position;
+            _theme = theme;
+        }
+
+        public string Name { get => _name; }
+    }
+}
