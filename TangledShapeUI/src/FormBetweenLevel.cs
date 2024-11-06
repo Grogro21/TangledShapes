f2862c99fc8d56fce750f7b187c7c0694c184902
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2024-11-06 08:43:09 +0100

    add:FormBetweenLevel

---
+++ TangledShapeUI/src/FormBetweenLevel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TangledShapeUI
+{
+    public partial class FormBetweenLevel : Form
+    {
+        public FormBetweenLevel(bool isPreviousLevelSuccess = false)
+        {
+            this.InitializeComponent();
+
+            this._lblResult.Text = isPreviousLevelSuccess ? "Bravo!" : "Dommage!";
+        }
+
+        private async void FormLoaded(object sender, EventArgs e)
+        {
+            await Task.Delay(5_000);
+
+            throw new NotImplementedException();
+        }
+    }
+}
