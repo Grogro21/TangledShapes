93234c3c580ec60528624cacd57b536de4a2807e
Grogro21 <74548874+Grogro21@users.noreply.github.com>
2024-11-07 12:20:39 +0100

    Update Shape.cs

--- AppFormes/ShapeSMOdel/src/Shape.cs
+++ AppFormes/ShapeSMOdel/src/Shape.cs
@@ -15,12 +15,9 @@ namespace ShapeSMOdel
             _theme = theme;
         }
 
-        public Shape(SelectableShape selectableShape, Point realPosition)
-        {
-            _name = selectableShape.GetName();
-            _theme = selectableShape.GetTheme();
-            _position = realPosition;
-        }
+        public Shape(SelectableShape selectableShape, Point realPosition) : this(selectableShape.GetName(), realPosition, selectableShape.GetTheme())
+        { }
+
         public string Name { get => _name; }
         public Point Position { get => _position; set => _position = value; }
         public string Theme { get => _theme; }
